9fc4e231f669f4f176405e86842dfdaa3cd6eab1
李文强 <codelove1314@live.cn>
2017-03-17 10:55:47 +0800

    更新包发布脚本

--- Magicodes.WeChat.SDK/pack.bat
+++ Magicodes.WeChat.SDK/pack.bat
@@ -1,8 +1,19 @@
-
-echo 包搜索字符串
+:: 包搜索字符串
 echo %1
-echo 项目方案地址
+:: 项目方案地址
 echo %2
 
-echo 打包
+:: 删除历史包
+del %1 /f /q /a 
+
+:: 包名称
+set nupkg=""
+
+:: 打包
 nuget Pack %2 -Build -Properties Configuration=Release
+
+:: 更新包名称
+for %%a in (dir /s /a /b "./%1") do (set nupkg=%%a)
+
+:: 推送包
+nuget push %nupkg% 4e3f500e-a031-45fa-b746-5762275e7561 -Source https://www.nuget.org/api/v2/package
