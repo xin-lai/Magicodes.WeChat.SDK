eec3cb43a991f9ab8ebc91c4268b9c3442d66695
代瓒 <1822802785@qq.com>
2018-11-14 11:38:09 +0800

    修改扫码登录参数

--- Magicodes.WeChat.SDK/Magicodes.WeChat.SDK.Core/Apis/OAuth/OAuthScopes.cs
+++ Magicodes.WeChat.SDK/Magicodes.WeChat.SDK.Core/Apis/OAuth/OAuthScopes.cs
@@ -14,6 +14,11 @@
         /// <summary>
         ///     弹出授权页面，可通过openid拿到昵称、性别、所在地。并且，即使在未关注的情况下，只要用户授权，也能获取其信息
         /// </summary>
-        snsapi_userinfo
+        snsapi_userinfo,
+
+        /// <summary>
+        ///     弹出扫码授权页面，可通过openid拿到昵称、性别、所在地。
+        /// </summary>
+        snsapi_login
     }
 }
