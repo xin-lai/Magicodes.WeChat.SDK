bbaf9b88d91d79ee2a9e67983b9c4c37b47b7a0a
李文强 <codelove1314@live.cn>
2018-12-10 13:42:26 +0800

    添加小程序默认配置类

---
+++ Magicodes.WeChat.SDK/Magicodes.WeChat.MiniProgram/Config/DefaultMiniProgramConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace Magicodes.WeChat.MiniProgram.Config
+{
+    /// <inheritdoc />
+    public class DefaultMiniProgramConfig : IMiniProgramConfig
+    {
+        /// <summary>
+        /// 小程序ID
+        /// </summary>
+        public string MiniProgramAppId { get; set; }
+
+        /// <summary>
+        /// 小程序密钥
+        /// </summary>
+        public string MiniProgramAppSecret { get; set; }
+    }
+}
