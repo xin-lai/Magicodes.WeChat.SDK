421f4fd3858714f580bfc0b7f80e90c9585ff954
李文强 <codelove1314@live.cn>
2019-07-30 19:57:05 +0800

    修改版本并推包

--- pack/pack.bat
+++ pack/pack.bat
@@ -19,4 +19,4 @@ dotnet pack %2 -c Release --output ../../pack/nupkgs
 for %%a in (dir /s /a /b "./nupkgs/%1") do (set nupkg=%%a)
 
 :: ÍÆËÍ°ü
-nuget push nupkgs/%nupkg% oy2jyui74772rfnnquwq3ydszczz6w5uhjpujhmjfnpaji -Source https://www.nuget.org/api/v2/package
+nuget push nupkgs/%nupkg% oy2l7xneus7n4i457xrqv5vnmtfgdnrexpjgtqyfznmrtu -Source https://www.nuget.org/api/v2/package
